b1f999af83472192623d0cf20843bbda6d50fdd8
Eyal Lotem <eyal.lotem@gmail.com>
2012-12-18 05:12:07 +0200

    CU now extracts producer and language

--- src/Data/Dwarf/ADT.hs
+++ src/Data/Dwarf/ADT.hs
@@ -1,10 +1,21 @@
 module Data.Dwarf.ADT (parseCU, CompilationUnit(..)) where
 
-import Data.Dwarf (DieID, DIE(..), DW_TAG(..))
+import Data.Dwarf (DieID, DIE(..), DW_TAG(..), DW_AT(..), DW_ATVAL(..), (!?))
 import qualified Data.Dwarf as Dwarf
+import qualified Data.Dwarf.Lens as Dwarf.Lens
 
+-- Example attributes
+-- DW_AT_producer=(DW_ATVAL_STRING "GNU C 4.4.5")
+-- DW_AT_language=(DW_ATVAL_UINT 1)
+-- DW_AT_name=(DW_ATVAL_STRING "../src/closures.c")
+-- DW_AT_comp_dir=(DW_ATVAL_STRING "/home/ian/zz/ghc-7.4.1/libffi/build/i386-unknown-linux-gnu")
+-- DW_AT_low_pc=(DW_ATVAL_UINT 135625548)
+-- DW_AT_high_pc=(DW_ATVAL_UINT 135646754)
+-- DW_AT_stmt_list=(DW_ATVAL_UINT 0)
 data CompilationUnit = CompilationUnit
   { cuDIE :: DieID
+  , producer :: String
+  , language :: Dwarf.DW_LANG
   } deriving (Eq, Ord, Read, Show)
 
 verifyTag :: DW_TAG -> DIE -> a -> a
@@ -14,7 +25,24 @@ verifyTag expected die x
   where
     tag = dieTag die
 
+uniqueAttr :: String -> DIE -> DW_AT -> DW_ATVAL
+uniqueAttr msg die at =
+  case die !? at of
+  [val] -> val
+  [] -> error $ msg ++ ": Missing value for attribute: " ++ show at
+  xs -> error $ msg ++ ": Multiple values for attribute: " ++ show at ++ ": " ++ show xs
+
+getAttrVal :: String -> DIE -> DW_AT -> Dwarf.Lens.ATVAL_NamedPrism a -> a
+getAttrVal container die at prism =
+  Dwarf.Lens.getATVal (container ++ " attribute " ++ show at) prism $
+  uniqueAttr container die at
+
 parseCU :: Dwarf.DIEMap -> DIE -> CompilationUnit
 parseCU _dieMap die =
-  verifyTag DW_TAG_compile_unit die .
-  CompilationUnit $ dieId die
+  verifyTag DW_TAG_compile_unit die $
+  CompilationUnit
+  (dieId die)
+  (getAttr DW_AT_producer Dwarf.Lens.aTVAL_STRING)
+  (Dwarf.dw_lang (getAttr DW_AT_language Dwarf.Lens.aTVAL_UINT))
+  where
+    getAttr = getAttrVal "Compilation unit" die
---
+++ src/Data/Dwarf/Lens.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Data.Dwarf.Lens
+  ( dW_ATVAL_INT, aTVAL_INT
+  , dW_ATVAL_UINT, aTVAL_UINT
+  , dW_ATVAL_REF, aTVAL_REF
+  , dW_ATVAL_STRING, aTVAL_STRING
+  , dW_ATVAL_BLOB, aTVAL_BLOB
+  , dW_ATVAL_BOOL, aTVAL_BOOL
+  , getATVal, ATVAL_NamedPrism
+  ) where
+
+import Control.Lens (Getting, (^?))
+import Control.Lens.TH (makePrisms)
+import Data.Dwarf (DieID, DW_ATVAL)
+import Data.Int (Int64)
+import Data.Maybe (fromMaybe)
+import Data.Word (Word64)
+import qualified Data.ByteString as BS
+import qualified Data.Monoid as Monoid
+
+type ATVAL_NamedPrism a = (String, Getting (Monoid.First a) DW_ATVAL DW_ATVAL a a)
+
+makePrisms ''DW_ATVAL
+
+aTVAL_INT :: ATVAL_NamedPrism Int64
+aTVAL_INT = ("ATVAL_INT", dW_ATVAL_INT)
+
+aTVAL_UINT :: ATVAL_NamedPrism Word64
+aTVAL_UINT = ("ATVAL_UINT", dW_ATVAL_UINT)
+
+aTVAL_REF :: ATVAL_NamedPrism DieID
+aTVAL_REF = ("ATVAL_REF", dW_ATVAL_REF)
+
+aTVAL_STRING :: ATVAL_NamedPrism String
+aTVAL_STRING = ("ATVAL_STRING", dW_ATVAL_STRING)
+
+aTVAL_BLOB :: ATVAL_NamedPrism BS.ByteString
+aTVAL_BLOB = ("ATVAL_BLOB", dW_ATVAL_BLOB)
+
+aTVAL_BOOL :: ATVAL_NamedPrism Bool
+aTVAL_BOOL = ("ATVAL_BOOL", dW_ATVAL_BOOL)
+
+getATVal :: String -> ATVAL_NamedPrism a -> DW_ATVAL -> a
+getATVal prefix (typName, typ) atval =
+  fromMaybe (error msg) $ atval ^? typ
+  where
+    msg = concat [prefix, " is: ", show atval, " but expected: ", typName]
